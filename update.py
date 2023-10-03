d45cec03985b8b7fb500219a3d7e38b4a20b2183
Maximilian Keßler <git@maximilian-kessler.de>
2023-10-03 14:21:45 +0200

    Also update IPv6 records

--- update.py
+++ update.py
@@ -54,9 +54,14 @@ def get_ip():
 
     return get("https://api.ipify.org/").content.decode("utf8")
 
+def get_ip6():
+    """Get public IPv6 address"""
 
-def get_records(config, domain: str, subdomain: str, record_type: list = ["A"]) -> list:
-    """Get current A records for a subdomain in a zone"""
+    return get("https://api6.ipify.org/").content.decode("utf8")
+
+
+def get_records(config, domain: str, subdomain: str, record_type: list = ["A", "AAAA"]) -> list:
+    """Get current records for a subdomain in a zone"""
 
     if not config or not domain:
         raise ValueError("Missing or invalid configuration parameters")
@@ -87,7 +92,7 @@ def get_records(config, domain: str, subdomain: str, record_type: list = ["A"]) 
         raise
 
 
-def add_record(config: dict, domain: str, subdomain: str, ip: str) -> bool:
+def add_record(config: dict, domain: str, subdomain: str, ip: str, record_type: str = "A") -> bool:
     """Add a new A record to a subdomains in a zone"""
 
     if not config or not domain or not subdomain or ip == "":
@@ -96,7 +101,7 @@ def add_record(config: dict, domain: str, subdomain: str, ip: str) -> bool:
     new_record = {
         "priority": "",
         "rdata": ip,
-        "type": "A",
+        "type": record_type,
         "ttl": config.get("ttl", 3600),
     }
 
@@ -119,12 +124,12 @@ def add_record(config: dict, domain: str, subdomain: str, ip: str) -> bool:
 def update_record(config: dict, domain: str, subdomain: str, ip: str, record: dict) -> int:
     """Update an existing A record for a subdomain in a zone"""
 
-    if record["rdata"] == new_ip and int(record["ttl"]) == int(config["ttl"]):
+    if record["rdata"] == ip and int(record["ttl"]) == int(config["ttl"]):
         return False
     new_record = {
         "priority": record["priority"],
         "record_id": record["record_id"],
-        "rdata": new_ip,
+        "rdata": ip,
         "type": record["type"],
         "ttl": config["ttl"],
     }
@@ -166,36 +171,36 @@ if __name__ == "__main__":
         client = xmlrpc.client.ServerProxy(uri="https://api.loopia.se/RPCSERV", encoding="utf-8")
         log.debug("Client initialized")
 
-        new_ip = get_ip()
-        log.debug(f"Current public ip is {new_ip}")
+        ipv4 = get_ip()
+        ipv6 = get_ip6()
+        log.debug(f"Current public ip is {ipv4} / {ipv6}")
 
         for subdomain in cfg['subdomains']:
-            fqdn = cfg["domain"] if subdomain == "@" else f"{subdomain}.{cfg['domain']}"
+            for record_type in ["A", "AAAA"]:
+                new_ip = ipv6 if record_type == "AAAA" else ipv4
+                fqdn = cfg["domain"] if subdomain == "@" else f"{subdomain}.{cfg['domain']}"
 
-            a_records = get_records(cfg, cfg['domain'], subdomain, record_type=["A"])
-            log.debug(f"Found {len(a_records)} A records in {fqdn}.")
+                a_records = get_records(cfg, cfg['domain'], subdomain, record_type=[record_type])
+                log.debug(f"Found {len(a_records)} {record_type} records in {fqdn}.")
+                try:
+                    if len(a_records) > 1:
+                        count_removed = clean_records(cfg, cfg['domain'], subdomain, a_records[1:])
+                        log.info(f"Cleaned up {count_removed} {record_type} records in {fqdn}")
 
-            try:
-                if len(a_records) > 1:
-                    count_removed = clean_records(cfg, cfg['domain'], subdomain, a_records[1:])
-                    log.info(f"Cleaned up {count_removed} A records in {fqdn}")
+                    elif len(a_records) == 0:
+                        if add_record(cfg, cfg['domain'], subdomain, new_ip, record_type):
+                            log.info(f"Added a new {record_type} record for {new_ip} in {fqdn}")
+                        else:
+                            log.critical(f"Unable to create {record_type} record in {fqdn}, exiting")
+                            exit(2)
 
-                elif len(a_records) == 0:
-                    if add_record(cfg, cfg['domain'], subdomain, new_ip):
-                        log.info(f"Added a new A record for {new_ip} in {fqdn}")
-                        exit(0)
                     else:
-                        log.critical(f"Unable to create record in {fqdn}, exiting")
-                        exit(2)
-
-                else:
-                    if update_record(cfg, cfg['domain'], subdomain, new_ip, a_records[0]):
-                        log.info(f"Updated the A record for {new_ip} in {fqdn}")
-                    else:
-                        log.info(f"A records for {fqdn} already up to date")
-            except Exception as e:
-                log.critical(e)
-                raise
-
+                        if update_record(cfg, cfg['domain'], subdomain, new_ip, a_records[0]):
+                            log.info(f"Updated the {record_type} record for {new_ip} in {fqdn}")
+                        else:
+                            log.info(f"A records for {fqdn} already up to date")
+                except Exception as e:
+                    log.critical(e)
+                    raise
     except Exception as e:
         exit(f"{e}")
